f7a3a234e16c1e3b8be288271398074c9b7d1e00
Chris Williams <diodesign@tuta.io>
2020-12-20 04:21:23 -0800

    Clear out empty tests and switch to write!()-based stdout

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,17 @@
  /* we're running in supervisor mode, not userland */
 #![no_std]
 #![no_main]
+#![allow(unused_must_use)]
 
-/* provide a framework for unit testing */
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::run_tests)]
-#![reexport_test_harness_main = "gooeytests"] /* entry point for tests */
+use core::fmt::Write;
 
 /* this will initialize and manage our supervisor-level environment */
-#[macro_use]
 extern crate supervisor;
+use supervisor::stdio::Stdout;
 
 #[no_mangle]
 pub extern "C" fn main()
 {
-    println!("hello world! I'm a system service");
-}
-
-#[cfg(test)]
-fn run_tests(unit_tests: &[&dyn Fn()])
-{
-    /* run each test one by one */
-    for test in unit_tests
-    {
-        test();
-    }
-
-    /* exit cleanly once tests are complete */
-    // platform::test::end(Ok(0));
-}
-
-#[test_case]
-fn test_assertion()
-{
-    assert_eq!(42, 42);
+    let mut stdout = Stdout::new();
+    write!(&mut stdout, "hello world! I'm a system service");
 }
