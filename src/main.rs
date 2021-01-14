3527088cd586214ecbb4cbce952ce8c840bbf3fb
Chris Williams <diodesign@tuta.io>
2021-01-14 01:16:48 -0800

    Use a lock to protect stdout when multi-threaded

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,28 @@
 #![no_main]
 #![allow(unused_must_use)]
 
-use core::fmt::Write;
+#[macro_use]
+extern crate lazy_static;
+extern crate spin;
+
+lazy_static!
+{
+    static ref STDOUT_LOCK: spin::Mutex<bool> = spin::Mutex::new(false);
+}
 
 /* this will initialize and manage our supervisor-level environment */
 extern crate supervisor;
 use supervisor::stdio::Stdout;
+use core::fmt::Write;
 
 #[no_mangle]
 pub extern "C" fn main()
 {
     let mut stdout = Stdout::new();
-    write!(&mut stdout, "hello world! I'm a system service\n");
+
+    {
+        let mut lock = STDOUT_LOCK.lock();
+        *lock = true;
+        write!(&mut stdout, "hello world! I'm a system service\n");
+    }
 }
